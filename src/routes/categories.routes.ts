d0770ce7c761bf6747c22c34e4465b5273aa8485
Leonardo Nakao <leonakao.dev@gmail.com>
2020-08-22 18:01:25 -0300

    Create categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,7 +3,19 @@ import { Router } from 'express';
 const categoriesRoutes = Router();
 
 categoriesRoutes.get('/', async (request, response) => {
-  return response.json({ ok: true });
+  return response.json({ development: true });
+});
+
+categoriesRoutes.put('/', async (request, response) => {
+  return response.json({ development: true });
+});
+
+categoriesRoutes.post('/', async (request, response) => {
+  return response.json({ development: true });
+});
+
+categoriesRoutes.delete('/', async (request, response) => {
+  return response.json({ development: true });
 });
 
 export default categoriesRoutes;
